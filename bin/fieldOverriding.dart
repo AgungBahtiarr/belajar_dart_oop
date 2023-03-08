5efd9c6b8567f8ba4a7ef6a24e2d1150f8011599
AgungBahtiarr <agungbahtiar050@gmail.com>
2023-03-08 10:19:01 +0700

    add fieldOverriding.dart

---
+++ bin/fieldOverriding.dart
@@ -0,0 +1,21 @@
+import 'dart:ffi';
+
+class Person {
+  String name = "Agung";
+
+  void sayHello(String name) {
+    print("Hello $name, my name is ${this.name}");
+  }
+}
+
+class OtherPerson extends Person {
+  String name = "Ilham";
+}
+
+void main(List<String> args) {
+  var person1 = Person();
+  person1.sayHello("heru");
+
+  var person2 = OtherPerson();
+  person2.sayHello("yogi");
+}
